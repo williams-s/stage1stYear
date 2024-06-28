64d011a620894a16420190474ba136fd79baa27c
williams-s <willay.anthony@gmail.com>
2024-06-28 22:16:17 +0200

    dazadz

--- swapping.js
+++ swapping.js
@@ -1,12 +1,12 @@
 const swicthBtn = document.getElementById('switch');
 swicthBtn.addEventListener('click', () => {
     console.log("clicked");
-    if (window.location.pathname === 'view_results.html'){
+    if (window.location.pathname === '/view_results.html'){
         console.log("view_results");
-        window.location.href = 'questions.html';
+        window.location.href = '/questions.html';
     }
-    else if (window.location.pathname === 'questions.html'){
+    else if (window.location.pathname === '/questions.html'){
         console.log("questions");
-        window.location.href = 'view_results.html';
+        window.location.href = '/view_results.html';
     }
 })
