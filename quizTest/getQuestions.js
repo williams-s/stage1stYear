2b99875150117da27215d68be35f89f9e1ef66e9
williams-s <willay.anthony@gmail.com>
2024-06-28 21:32:22 +0200

    eza

--- quizTest/getQuestions.js
+++ quizTest/getQuestions.js
@@ -2,17 +2,22 @@ fetch('getQuestions.php')
     .then(response => response.json())
     .then(data => {
         const questionsContainer = document.getElementById('questionContainer');
-        data.forEach(question => {
+        
+        data.forEach((question, index) => {
             const questionContainer = document.createElement('div');
             questionContainer.className = 'question-container';
             questionContainer.id = `question${question.id}`;
             questionContainer.innerHTML = `
                 <p class="question">${question.question}</p>
                 <div class="btn-container">
-                    <button class="btn-next" onclick="nextQuestion('question${question.id + 1}')">Suivant</button>
+                    ${index < data.length - 1 ? `
+                        <button class="btn-next" onclick="nextQuestion('question${data[index + 1].id}')">Suivant</button>
+                    ` : `
+                        <button class="btn-next" disabled>Fin du quiz</button>
+                    `}
                 </div>
             `;
             questionsContainer.appendChild(questionContainer);
         });
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
