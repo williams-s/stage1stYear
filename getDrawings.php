202d9b84bdc7213190b9d7fed79b854ecc5f48be
williams-s <willay.anthony@gmail.com>
2024-06-28 05:30:46 +0200

    a

---
+++ getDrawings.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
+
+$config = Yaml::parseFile('config.yaml');
+
+$servername = $config['database']['host'];
+$username = $config['database']['username'];
+$password = $config['database']['password'];
+$dbname = $config['database']['dbname'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connexion échouée : " . $conn->connect_error);
+}
+
+$sql = "SELECT username, photo_url FROM users";
+$result = $conn->query($sql);
+
+$todo = [];
+
+if ($result->num_rows > 0) {
+    // Récupérer les données de chaque ligne
+    while($row = $result->fetch_assoc()) {
+        $todo[] = $row;
+    }
+}
+
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode($todo);
+exit;
+?>
---
+++ view_results.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const filePath = urlParams.get('filePath');
+    if (filePath) {
+        document.getElementById('canvas-image').src = filePath;
+    }
+});
+
+
+const results_players = document.getQuerySelector('.results-players');
+function getDrawings(){
+    fetch('view_results.php')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(user => {
+            let img = document.createElement('src');
+            img.src = user.photo_url;
+            img.id = "canvas-image";
+            let user_name = document.createElement('p');
+            user_name.textContent = user.user_name;
+            results_players.appendChild(img);
+            results_players.appendChild(user_name);
+        });
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+setInterval(getDrawings, 5000);
+
+getDrawings();
+
+
+
+
+
