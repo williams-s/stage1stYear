9897c0c5b80efb7cf9e38ad2d33fb2f74717f2ed
williams-s <willay.anthony@gmail.com>
2024-06-28 21:34:23 +0200

    fezfeaz

--- quizTest/getQuestions.php
+++ quizTest/getQuestions.php
@@ -14,12 +14,19 @@ $username = $config['database']['username'];
 $password = $config['database']['password'];
 $dbname = $config['database']['dbname'];
 
+// Connexion à la base de données
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Vérifier la connexion
 if ($conn->connect_error) {
-    die("Connexion échouée : " . $conn->connect_error);
+    // Gérer l'erreur de connexion
+    $error = ["error" => "Connexion échouée : " . $conn->connect_error];
+    header('Content-Type: application/json');
+    echo json_encode($error);
+    exit;
 }
 
+// Requête SQL pour récupérer les données
 $sql = "SELECT * FROM quiz";
 $result = $conn->query($sql);
 
@@ -27,13 +34,14 @@ $todo = [];
 
 if ($result->num_rows > 0) {
     // Récupérer les données de chaque ligne
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         $todo[] = $row;
     }
 }
 
 $conn->close();
 
+// Renvoyer les données sous forme de JSON
 header('Content-Type: application/json');
 echo json_encode($todo);
 exit;
