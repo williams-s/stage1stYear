f0c011432e7453926ea11a3decc8215e0fceeea8
williams-s <willay.anthony@gmail.com>
2024-06-28 21:36:35 +0200

    fae

--- quizTest/getQuestions.js
+++ quizTest/getQuestions.js
@@ -1,23 +1,31 @@
-fetch('getQuestions.php')
-    .then(response => response.json())
-    .then(data => {
-        const questionsContainer = document.getElementById('questionContainer');
-        
-        data.forEach((question, index) => {
-            const questionContainer = document.createElement('div');
-            questionContainer.className = 'question-container';
-            questionContainer.id = `question${question.id}`;
-            questionContainer.innerHTML = `
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("getQuestions.php")
+    .then((response) => response.json())
+    .then((data) => {
+      const questionsContainer = document.getElementById("questionContainer");
+
+      data.forEach((question, index) => {
+        const questionContainer = document.createElement("div");
+        questionContainer.className = "question-container";
+        questionContainer.id = `question${question.id}`;
+        questionContainer.innerHTML = `
                 <p class="question">${question.question}</p>
                 <div class="btn-container">
-                    ${index < data.length - 1 ? `
-                        <button class="btn-next" onclick="nextQuestion('question${data[index + 1].id}')">Suivant</button>
-                    ` : `
+                    ${
+                      index < data.length - 1
+                        ? `
+                        <button class="btn-next" onclick="nextQuestion('question${
+                          data[index + 1].id
+                        }')">Suivant</button>
+                    `
+                        : `
                         <button class="btn-next" disabled>Fin du quiz</button>
-                    `}
+                    `
+                    }
                 </div>
             `;
-            questionsContainer.appendChild(questionContainer);
-        });
+        questionsContainer.appendChild(questionContainer);
+      });
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
+});
